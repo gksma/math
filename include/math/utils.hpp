47311035ff8ca4cf337fe26a57141b6de63a392f
gksma <dhfz1794@naver.com>
2023-12-28 15:58:59 +0900

    Added skeleton code for math repo

---
+++ include/math/utils.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 gksma, All Rights Reserved.
+// Authors: HanHeum Cho
+
+#ifndef GKSMA__MATH__UTILS_HPP_
+#define GKSMA__MATH__UTILS_HPP_
+
+namespace gksma::math {
+// Add, Subtract, Multiply, Divide
+
+// IsEqual
+}   // namespace gksma::math
+
+#endif
